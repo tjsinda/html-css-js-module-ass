72f8032c68325c8ea64979d91780bab4bfa87588
sinda <you@example.com>
2024-10-22 07:28:10 +0530

    add newfolder file content

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector('.cvdownload').addEventListener('click', cvhandler)
+
+function cvhandler() {
+    alert('Cv is not updated, will add later')
+}
+function handlervalidation() {
+    
+    window.open
+}
+
+function handlerCalculator() {
+    window.open("https://online-calculator-demo-intrainz.netlify.app/", '_blank')
+}
+
+function handlerShop() {
+    window.open("https://tjsinda.github.io/Bootstrap-assignment/", '_blank')
+}
+
+function validation() {
+    event.preventDefault();
+    setTimeout(() => {
+        alert("Thanks, Will contact you")
+    },500);
+    document.querySelector('.resultvalidation').innerHTML = 'Your message has been send successfully'
+}
